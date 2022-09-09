f652598ecda0bd4cc9748372d835e40e69eb6510
Adrian Stockwell <adrian_stocky_@hotmail.com>
2022-09-10 09:47:19 +1000

    updated gold divison

--- natio_mine_backend/profile_api/miner.py
+++ natio_mine_backend/profile_api/miner.py
@@ -16,7 +16,7 @@ class Miner():
     def calculate_experience(self, profile):
         mining_actions_left = 5
         new_experience_level = 0
-        gold_stash = 0
+        current_gold_stash = profile.gold
         current_profile = profile
         while(mining_actions_left > 0):
             time.sleep(self.generate_random_time())
@@ -24,8 +24,8 @@ class Miner():
             new_experience_level += random_range
             mining_actions_left -= 1
             gold_found = self.find_gold()
-            gold_stash += gold_found
-        return {"experience": current_profile.experience + new_experience_level, "gold": gold_stash}
+            current_gold_stash += gold_found
+        return {"experience": current_profile.experience + new_experience_level, "gold": current_gold_stash}
 
     def run_mining_operation(self, profile):
         experience_result = self.calculate_experience(profile)
